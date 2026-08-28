81068832b7ad3e699a07de1f2febe6f822efc671
robymasia <35657567+robymasia@users.noreply.github.com>
2026-08-28 20:18:10 +0200

    Add presets package init
    - Creato presets/__init__.py per esportare i preset
    - Importa DUB_CLASSIC_PRESET, DUB_PRESETS_BY_BPM e calculate_delay_time

---
+++ presets/__init__.py
@@ -0,0 +1,9 @@
+# Package presets per dub-station-freqq
+# 
+# Utilizzo:
+#   from presets import DUB_CLASSIC_PRESET
+#   from presets.dub_classic import calculate_delay_time
+
+from presets.dub_classic import DUB_CLASSIC_PRESET, DUB_PRESETS_BY_BPM, calculate_delay_time
+
+__all__ = ["DUB_CLASSIC_PRESET", "DUB_PRESETS_BY_BPM", "calculate_delay_time"]
