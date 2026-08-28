b687436fdcaf85a143a3d4a05e59859a098f2972
robymasia <35657567+robymasia@users.noreply.github.com>
2026-08-28 20:19:19 +0200

    Update presets/__init__.py per esportare PresetLoader
    - Aggiunto PresetLoader e get_dub_preset_for_bpm alle esportazioni

--- presets/__init__.py
+++ presets/__init__.py
@@ -1,9 +1,16 @@
 # Package presets per dub-station-freqq
 # 
 # Utilizzo:
-#   from presets import DUB_CLASSIC_PRESET
+#   from presets import DUB_CLASSIC_PRESET, PresetLoader
 #   from presets.dub_classic import calculate_delay_time
 
 from presets.dub_classic import DUB_CLASSIC_PRESET, DUB_PRESETS_BY_BPM, calculate_delay_time
+from presets.preset_loader import PresetLoader, get_dub_preset_for_bpm
 
-__all__ = ["DUB_CLASSIC_PRESET", "DUB_PRESETS_BY_BPM", "calculate_delay_time"]
+__all__ = [
+    "DUB_CLASSIC_PRESET",
+    "DUB_PRESETS_BY_BPM",
+    "calculate_delay_time",
+    "PresetLoader",
+    "get_dub_preset_for_bpm"
+]
